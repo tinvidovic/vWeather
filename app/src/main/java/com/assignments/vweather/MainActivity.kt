4b0efa651c019a60d2c3f885c4dfb50a8a501a13
tinvidovic <tin@vidovic.family>
2024-03-27 21:05:28 +0100

    Remove generated code

--- app/src/main/java/com/assignments/vweather/MainActivity.kt
+++ app/src/main/java/com/assignments/vweather/MainActivity.kt
@@ -6,10 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.assignments.vweather.ui.theme.VWeatherTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,28 +16,9 @@ class MainActivity : ComponentActivity() {
             VWeatherTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
+                ) {}
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    VWeatherTheme {
-        Greeting("Android")
     }
 }
