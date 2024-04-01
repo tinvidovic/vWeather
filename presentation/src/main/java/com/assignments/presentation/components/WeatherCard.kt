b4f38939396fbb952b762eb7fcb89dac0fa9b122
tinvidovic <tin@vidovic.family>
2024-04-01 15:10:16 +0200

    New WeatherCard design

--- presentation/src/main/java/com/assignments/presentation/components/WeatherCard.kt
+++ presentation/src/main/java/com/assignments/presentation/components/WeatherCard.kt
@@ -5,6 +5,8 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -24,6 +26,7 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
@@ -66,9 +69,55 @@ fun WeatherCard(
                         )
                     )
                 )
+                .fillMaxWidth(),
+            contentAlignment = Alignment.Center
         ){
 
-            Row(
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+
+                Text(
+                    text = uiWeather.cityName,
+                    style = largeStyle,
+                    textAlign = TextAlign.Center,
+                )
+
+                AsyncImage(
+                    model = ImageRequest.Builder(LocalContext.current)
+                        .data(uiWeather.iconUrl)
+                        .crossfade(true)
+                        .build(),
+                    placeholder = painterResource(R.drawable.ic_default_weather),
+                    error = painterResource(id = R.drawable.ic_default_weather),
+                    fallback = painterResource(id = R.drawable.ic_default_weather),
+                    colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.onPrimary),
+                    contentDescription = null,
+                    clipToBounds = true
+
+                )
+
+                Text(
+                    text = uiWeather.name,
+                    style = mediumStyle,
+                    textAlign = TextAlign.Center,
+                )
+
+                Text(
+                    text = "${uiWeather.currentTemperature}$unitCode",
+                    style = largeStyle,
+                    modifier = Modifier.padding(end = 8.dp),
+                    textAlign = TextAlign.Center
+                )
+
+                Text(
+                    text = "${uiWeather.maxTemperature}$unitCode / ${uiWeather.minTemperature}$unitCode",
+                    style = smallStyle,
+                    textAlign = TextAlign.Center
+                )
+            }
+
+            /*Row(
                 verticalAlignment = Alignment.CenterVertically,
                 horizontalArrangement = Arrangement.SpaceAround,
                 modifier = modifier
@@ -143,7 +192,7 @@ fun WeatherCard(
                         .size(68.dp)
 
                 )
-            }
+            }*/
 
         }
     }
